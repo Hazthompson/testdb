b8beae3525e4781c92ddc0d87be2bce0328bdccc
Harriet Thompson <hazthomps@yahoo.co.uk>
2019-05-22 02:23:18 +0000

    knex function added to insert into out famous_people table

--- add_person.js
+++ add_person.js
@@ -1,3 +1,4 @@
+const settings = require("./settings");
 var knex = require('knex')({
   client: 'pg',
   // version: '7.2',
@@ -14,20 +15,15 @@ const lastName = process.argv[3];
 const date = process.argv[4];
 
 
-// knex.select('*').from('famous_people')
-//   .whereRaw('first_name = ? OR last_name = ? ', [firstName, lastName, date])
-//   .asCallback(function(err, rows) {
-//     if (err) {
-//         console.log("error:", err);
-//       }
+       knex('famous_people').insert([
+          {first_name: firstName, //if this is in separate objects then each will be a separate row.
+          last_name: lastName,
+           birthdate: date }])
+       .asCallback(function(err, rows) {
+    if (err) {
+        console.log("error:", err);
+      } else {
+        console.log("success")
+      }
+    });
 
-
-    // knex.schema.createTable('famous_people', (table) => {
-    //     table.increments('id')
-    //     table.string('first_name')
-    //     table.string('last_name')
-    // })
-    // Make the change
-
-};
-  });
