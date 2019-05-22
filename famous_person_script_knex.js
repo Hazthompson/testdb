77fc06d09df991a3c98b12da603e43bb0b1a14e2
Harriet Thompson <hazthomps@yahoo.co.uk>
2019-05-22 03:16:26 +0000

    migration added, first migration added and run to create new table

--- famous_person_script_knex.js
+++ famous_person_script_knex.js
@@ -1,6 +1,5 @@
 
 const settings = require("./settings");
-//move the info over?
 
 var knex = require('knex')({
   client: 'pg',
@@ -14,8 +13,6 @@ var knex = require('knex')({
 });
 
 var someName = process.argv[2];
-
-//why just rows below?
 
 knex.select('*').from('famous_people')
   .whereRaw('first_name = ? OR last_name = ? ', [someName, someName])
@@ -34,24 +31,3 @@ knex.select('*').from('famous_people')
   });
 
 
-
-
-  // function getFamousPerson(arg) {
-  // client.query(
-  //   "SELECT * FROM famous_people WHERE first_name = $1 OR last_name = $1",
-  //   [someName],
-  //   (err, res) => {
-  //     if (err) {
-  //       console.log("error:", err);
-  //     } else {
-  //       for (var object of res.rows) {
-  //         let valuesString = Object.values(object);
-  //         string3 = String(valuesString[3]);
-  //         let newstring =
-  //         `${valuesString[0]}: ${valuesString[1]}, ${valuesString[2]}, born ${string3.slice(0, -24)}`;
-  //         console.log(newstring);
-  //       }
-  //       client.end();
-  //     }
-  //   }
-  // );
